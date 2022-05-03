afdfaf68b55f3f9388c396490d8fc35a2b5c70a8
Pedro Colombano <pedromecf@gmail.com>
2022-05-03 14:47:22 -0300

    Project created

---
+++ src/main/java/br/com/lacostech/pegasusbackend/PegasusBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.lacostech.pegasusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegasusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegasusBackendApplication.class, args);
+	}
+
+}
