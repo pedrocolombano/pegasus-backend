efdcca4930be1f6af1c39dc611fa7d944776460a
Pedro Colombano <pedromecf@gmail.com>
2022-05-22 20:00:12 -0300

    Update category

--- src/main/java/br/com/lacostech/pegasusbackend/controllers/CategoryController.java
+++ src/main/java/br/com/lacostech/pegasusbackend/controllers/CategoryController.java
@@ -5,7 +5,9 @@ import br.com.lacostech.pegasusbackend.services.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +37,12 @@ public class CategoryController {
                 .buildAndExpand(response.getId())
                 .toUri();
         return ResponseEntity.created(uri).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryModel> update(@PathVariable final Long id, @RequestBody final CategoryModel request) {
+        CategoryModel response = categoryService.update(id, request);
+        return ResponseEntity.ok(response);
     }
 
 }
--- src/main/java/br/com/lacostech/pegasusbackend/services/CategoryService.java
+++ src/main/java/br/com/lacostech/pegasusbackend/services/CategoryService.java
@@ -3,6 +3,7 @@ package br.com.lacostech.pegasusbackend.services;
 import br.com.lacostech.pegasusbackend.model.CategoryModel;
 import br.com.lacostech.pegasusbackend.model.entities.Category;
 import br.com.lacostech.pegasusbackend.repositories.CategoryRepository;
+import br.com.lacostech.pegasusbackend.services.exceptions.NotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +28,15 @@ public class CategoryService {
     @Transactional
     public CategoryModel insert(final CategoryModel request) {
         Category category = new Category();
+        copyDataFromRequest(request, category);
+        category = categoryRepository.save(category);
+        return new CategoryModel(category);
+    }
+
+    @Transactional
+    public CategoryModel update(final Long id, final CategoryModel request) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Category id " + id + " not found"));
         copyDataFromRequest(request, category);
         category = categoryRepository.save(category);
         return new CategoryModel(category);
