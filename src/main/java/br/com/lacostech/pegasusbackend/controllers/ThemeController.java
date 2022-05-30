6ca59ee8b775ced454903eb301bd2c02095e6eed
Pedro Colombano <pedromecf@gmail.com>
2022-05-30 00:57:53 -0300

    Insert themes

--- src/main/java/br/com/lacostech/pegasusbackend/controllers/ThemeController.java
+++ src/main/java/br/com/lacostech/pegasusbackend/controllers/ThemeController.java
@@ -5,9 +5,13 @@ import br.com.lacostech.pegasusbackend.services.ThemeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +25,16 @@ public class ThemeController {
     public ResponseEntity<List<ThemeModel>> findAll() {
         List<ThemeModel> themes = themeService.findAll();
         return ResponseEntity.ok(themes);
+    }
+
+    @PostMapping
+    public ResponseEntity<ThemeModel> insert(@RequestBody final ThemeModel request) {
+        ThemeModel response = themeService.insert(request);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(response);
     }
 
 }
--- src/main/java/br/com/lacostech/pegasusbackend/services/ThemeService.java
+++ src/main/java/br/com/lacostech/pegasusbackend/services/ThemeService.java
@@ -1,12 +1,15 @@
 package br.com.lacostech.pegasusbackend.services;
 
 import br.com.lacostech.pegasusbackend.model.ThemeModel;
+import br.com.lacostech.pegasusbackend.model.entities.Theme;
 import br.com.lacostech.pegasusbackend.repositories.ThemeRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,6 +24,21 @@ public class ThemeService {
                 .stream()
                 .map(ThemeModel::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public ThemeModel insert(final ThemeModel request) {
+        Theme theme = new Theme();
+        copyDataFromRequest(request, theme);
+
+        theme = themeRepository.save(theme);
+        return new ThemeModel(theme);
+    }
+
+    private void copyDataFromRequest(final ThemeModel request, final Theme entity) {
+        if (Objects.nonNull(request)) {
+            BeanUtils.copyProperties(request, entity);
+        }
     }
 
 }
