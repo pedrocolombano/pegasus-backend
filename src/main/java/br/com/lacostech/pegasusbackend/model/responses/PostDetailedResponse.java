1df846dd771129979c2275de78eca41c29a12414
Pedro Colombano <pedromecf@gmail.com>
2022-05-29 23:22:01 -0300

    Post detailed response alteration

--- src/main/java/br/com/lacostech/pegasusbackend/model/responses/PostDetailedResponse.java
+++ src/main/java/br/com/lacostech/pegasusbackend/model/responses/PostDetailedResponse.java
@@ -1,38 +1,23 @@
 package br.com.lacostech.pegasusbackend.model.responses;
 
 import br.com.lacostech.pegasusbackend.model.entities.Post;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.BeanUtils;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
-public class PostDetailedResponse implements Serializable {
+public class PostDetailedResponse extends PostResponse {
 
-    private static final long serialVersionUID = 1L;
-
-    private Long id;
-    private String mainImageUrl;
-    private String title;
-    private String summary;
-    private ThemeResponse theme;
     private final List<ArticleResponse> articles = new ArrayList<>();
     private final List<String> images = new ArrayList<>();
 
     public PostDetailedResponse(final Post entity) {
+        super(entity);
         if (Objects.nonNull(entity)) {
-            BeanUtils.copyProperties(entity, this);
-            this.theme = new ThemeResponse(entity.getTheme());
-
             entity.getArticles().forEach(art -> this.articles.add(new ArticleResponse(art)));
             this.images.addAll(entity.getImages());
         }
