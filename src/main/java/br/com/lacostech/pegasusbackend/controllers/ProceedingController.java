61d391b3670975a03105c4e6900fee2c127349e6
Pedro Colombano <pedromecf@gmail.com>
2022-05-30 18:06:41 -0300

    Insert proceeding

--- src/main/java/br/com/lacostech/pegasusbackend/controllers/ProceedingController.java
+++ src/main/java/br/com/lacostech/pegasusbackend/controllers/ProceedingController.java
@@ -5,9 +5,13 @@ import br.com.lacostech.pegasusbackend.services.ProceedingService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +25,16 @@ public class ProceedingController {
     public ResponseEntity<List<ProceedingModel>> findAll() {
         List<ProceedingModel> proceedings = proceedingService.findAll();
         return ResponseEntity.ok(proceedings);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProceedingModel> insert(@RequestBody final ProceedingModel request) {
+        ProceedingModel response = proceedingService.insert(request);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(response);
     }
 
 }
--- src/main/java/br/com/lacostech/pegasusbackend/services/ProceedingService.java
+++ src/main/java/br/com/lacostech/pegasusbackend/services/ProceedingService.java
@@ -1,12 +1,14 @@
 package br.com.lacostech.pegasusbackend.services;
 
 import br.com.lacostech.pegasusbackend.model.ProceedingModel;
+import br.com.lacostech.pegasusbackend.model.entities.Proceeding;
 import br.com.lacostech.pegasusbackend.repositories.ProceedingRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,6 +24,22 @@ public class ProceedingService {
                 .stream()
                 .map(ProceedingModel::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public ProceedingModel insert(final ProceedingModel request) {
+        Proceeding proceeding = new Proceeding();
+        copyDataFromRequest(request, proceeding);
+
+        proceeding = proceedingRepository.save(proceeding);
+        return new ProceedingModel(proceeding);
+    }
+
+    private void copyDataFromRequest(final ProceedingModel request, final Proceeding entity) {
+        if (Objects.nonNull(request)) {
+            entity.setName(request.getName());
+            entity.setPrice(request.getPrice());
+        }
     }
 
 }
